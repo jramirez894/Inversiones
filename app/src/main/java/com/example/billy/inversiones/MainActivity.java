1ba9716b09bf5c497ca6c525e49bf4f56758e310
jramirez894 <jramirez894@misena.edu.co>
2015-09-22 12:56:55 -0500

    borre el texview del layout

--- app/src/main/java/com/example/billy/inversiones/MainActivity.java
+++ app/src/main/java/com/example/billy/inversiones/MainActivity.java
@@ -1,20 +1,48 @@
 package com.example.billy.inversiones;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
+    EditText cedula;
+    EditText contrasena;
+
+    Button ingresar;
+    TextView olvidar;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        cedula =(EditText)findViewById(R.id.editTextCedula);
+        contrasena =(EditText)findViewById(R.id.editTextContrasena);
+        ingresar=(Button)findViewById(R.id.buttonLogin);
+        olvidar=(TextView)findViewById(R.id.texViewOlvidarContrasena);
+
+        olvidar.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Intent intent=new Intent(MainActivity.this,OlvidarContrasena.class);
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
@@ -28,7 +56,8 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_settings)
+        {
             return true;
         }
 
--- app/src/main/java/com/example/billy/inversiones/OlvidarContrasena.java
+++ app/src/main/java/com/example/billy/inversiones/OlvidarContrasena.java
@@ -1,27 +1,91 @@
 package com.example.billy.inversiones;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
 
-public class OlvidarContrasena extends AppCompatActivity {
+public class OlvidarContrasena extends AppCompatActivity
+{
+    EditText correo;
+    Button enviar;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_olvidar_contrasena);
+
+        ActionBar actionBar =getSupportActionBar();
+        actionBar.setTitle("Volver");
+
+        correo=(EditText)findViewById(R.id.editTextCorreo_OlvidarContrasena);
+        enviar=(Button)findViewById(R.id.buttonEnviar_OlvidarContrasena);
+
+        enviar.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                String capCorreo =correo.getText().toString();
+                if (capCorreo.equals(""))
+                {
+                    Toast.makeText(OlvidarContrasena.this,"Escriba su Correo",Toast.LENGTH_SHORT).show();
+                }
+                else
+                {
+                    AbrirAlerta();
+                }
+
+            }
+        });
+    }
+
+    public void AbrirAlerta()
+    {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setIcon(android.R.drawable.ic_menu_save);
+        builder.setTitle("Enviar");
+        builder.setMessage("¿Enviar Codigo de Recuperación?");
+        builder.setPositiveButton("Aceptar", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Intent intent = new Intent(OlvidarContrasena.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        builder.setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Intent intent = new Intent(OlvidarContrasena.this, OlvidarContrasena.class);
+                startActivity(intent);
+            }
+        });
+        builder.show();
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_olvidar_contrasena, menu);
         return true;
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
