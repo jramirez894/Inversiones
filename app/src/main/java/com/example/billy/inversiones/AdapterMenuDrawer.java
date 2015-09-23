1f962e93f4b4b526b030bb72f7360ae5547e505b
miguelc1234 <maorejuela@misena.edu.co>
2015-09-23 08:53:29 -0500

    Cambios

---
+++ app/src/main/java/com/example/billy/inversiones/AdapterMenuDrawer.java
@@ -0,0 +1,42 @@
+package com.example.billy.inversiones;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
+/**
+ * Created by Miguel on 23/09/2015.
+ */
+public class AdapterMenuDrawer extends ArrayAdapter {
+
+    public AdapterMenuDrawer(Context context, List objects) {
+        super(context, 0, objects);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        if(convertView == null){
+            LayoutInflater inflater = (LayoutInflater)parent.getContext().
+                    getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+            convertView = inflater.inflate(R.layout.lista_menu_drawer, null);
+        }
+
+
+        ImageView icon = (ImageView) convertView.findViewById(R.id.icono);
+        TextView name = (TextView) convertView.findViewById(R.id.nombre);
+
+        ItemsMenuDrawer item = (ItemsMenuDrawer) getItem(position);
+        icon.setImageResource(item.getIcono());
+        name.setText(item.getNombre());
+
+        return convertView;
+    }
+}
--- app/src/main/java/com/example/billy/inversiones/PrincipalMenu.java
+++ app/src/main/java/com/example/billy/inversiones/PrincipalMenu.java
@@ -1,16 +1,107 @@
 package com.example.billy.inversiones;
 
+import android.support.v4.app.ActionBarDrawerToggle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
-public class PrincipalMenu extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class PrincipalMenu extends AppCompatActivity
+{
+    private DrawerLayout menuDrawer;
+    private ActionBarDrawerToggle toggle;
+    ListView listaDrawer;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal_menu);
+
+        ActionBar actionBar = getSupportActionBar();
+
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.invalidateOptionsMenu();
+        actionBar.setTitle("Menu");
+        actionBar.setHomeAsUpIndicator(R.drawable.ic_drawer);
+
+        String[] titulos = getResources().getStringArray(R.array.array_menu_drawer);
+
+        //Obtener drawer
+        menuDrawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+
+        //Obtener listview
+        listaDrawer = (ListView) findViewById(R.id.lista_menu_drawer);
+
+        ArrayList<ItemsMenuDrawer> items = new ArrayList<ItemsMenuDrawer>();
+        items.add(new ItemsMenuDrawer(titulos[0], R.mipmap.ic_launcher));
+        items.add(new ItemsMenuDrawer(titulos[1], R.mipmap.ic_launcher));
+        items.add(new ItemsMenuDrawer(titulos[2], R.mipmap.ic_launcher));
+        items.add(new ItemsMenuDrawer(titulos[3], R.mipmap.ic_launcher));
+        items.add(new ItemsMenuDrawer(titulos[4], R.mipmap.ic_launcher));
+        items.add(new ItemsMenuDrawer(titulos[5], R.mipmap.ic_launcher));
+        items.add(new ItemsMenuDrawer(titulos[6], R.mipmap.ic_launcher));
+        items.add(new ItemsMenuDrawer(titulos[7], R.mipmap.ic_launcher));
+        items.add(new ItemsMenuDrawer(titulos[8], R.mipmap.ic_launcher));
+
+        // Relacionar el adaptador y la escucha de la lista del drawer
+        listaDrawer.setAdapter(new AdapterMenuDrawer(this, items));
+
+        //Activar icono del menu que se despliega
+        toggle = new ActionBarDrawerToggle(PrincipalMenu.this, menuDrawer, R.drawable.ic_drawer, R.string.drawer_inicio, R.string.drawer_fin);
+        getSupportActionBar().setHomeButtonEnabled(true);
+
+        listaDrawer.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int posicion, long l)
+            {
+                switch (posicion)
+                {
+                    case 0:
+
+                        break;
+
+                    case 1:
+
+                        break;
+
+                    case 2:
+
+                        break;
+
+                    case 3:
+
+                        break;
+
+                    case 4:
+
+                        break;
+
+                    case 5:
+
+                        break;
+
+                    case 6:
+
+                        break;
+
+                    case 7:
+
+                        break;
+
+                    case 8:
+
+                        break;
+                }
+            }
+        });
     }
 
     @Override
