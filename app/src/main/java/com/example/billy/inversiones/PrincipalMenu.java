ee3c027ca02d3e840f7bebd535cb7849d405b837
miguelc1234 <maorejuela@misena.edu.co>
2015-09-23 08:21:38 -0500

    Menu principal creado

--- app/src/main/java/com/example/billy/inversiones/PrincipalMenu.java
+++ app/src/main/java/com/example/billy/inversiones/PrincipalMenu.java
@@ -5,18 +5,18 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MenuPrincipal extends AppCompatActivity {
+public class PrincipalMenu extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_menu_principal);
+        setContentView(R.layout.activity_principal_menu);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_menu_principal, menu);
+        getMenuInflater().inflate(R.menu.menu_principal_menu, menu);
         return true;
     }
 
